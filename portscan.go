49456b27533b4569f99139aaaa4b2daddc60bcbb
Dr. Adedayo Adetoye <dayo.dev@gmail.com>
2019-01-21 21:08:53 +0000

    test AWS packet listening. Take 5

--- portscan.go
+++ portscan.go
@@ -341,11 +341,13 @@ func listenForEthernetPackets(handle *pcap.Handle) <-chan net.HardwareAddr {
 	output := make(chan net.HardwareAddr)
 	go func() {
 		var eth layers.Ethernet
+		// var ip layers.IPv4
 		parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth)
+		// parser := gopacket.NewDecodingLayerParser(layers.LayerTypeIPv4, &ip)
 		decodedLayers := []gopacket.LayerType{}
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
-			// fmt.Printf("Packet: %#v\n", packet)
+			fmt.Printf("Packet: %#v\n", packet)
 			parser.DecodeLayers(packet.Data(), &decodedLayers)
 			for _, lyr := range decodedLayers {
 				//Look for Ethernet frames
