7158ba97ec926f502257ac7d4f30271a976f2fa6
Dr. Adedayo Adetoye <dayo.dev@gmail.com>
2019-02-06 13:58:27 +0000

    Not terminating on CentOS, try conditional compile .3

--- pcap_support_darwin.go
+++ pcap_support_darwin.go
@@ -1,7 +1,16 @@
 package portscan
 
-import "github.com/google/gopacket/pcap"
+import (
+	"os"
+	"runtime/pprof"
+
+	"github.com/google/gopacket/pcap"
+)
 
 func closeHandle(handle *pcap.Handle) {
+	pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 	handle.Close()
+
+	println("After close")
+	pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
 }
